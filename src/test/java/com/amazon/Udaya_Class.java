47020456ede8e35669735450217a14db5b7d1d96
UdayaMarvel <udayamarvel@gmail.com>
2023-12-09 20:13:01 +0530

    Udaya

--- src/test/java/com/amazon/Udaya_Class.java
+++ src/test/java/com/amazon/Udaya_Class.java
@@ -3,6 +3,14 @@ package com.amazon;
 public class Udaya_Class {
 
 	public static void main(String[] args) {
-		System.out.println("Updated By Udaya");
+		String s = "Udaya";
+		String rev = "";
+		for(int i = 0;i<s.length();i++){
+			char character = s.charAt(i);
+			rev= character+rev;
+		}
+		System.out.println(rev);
+		
+		System.out.println("aiodaoiad");
 	}
 }
